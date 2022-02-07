fb3e27d5f2910ea540cacf4cc3510f2a44116ef0
m8eight <alekskoch234@gmail.com>
2022-02-07 23:35:29 +0300

    2.1.0 add comments in all files, better good structure

--- index.js
+++ index.js
@@ -4,40 +4,49 @@
  *MIT Licensed
 */
 
+var fileVerification = require('./lib/fileVerification.js');
+var pptxFactory = require('./lib/pptxFactory.js');
+
 const express = require('express');
 const app = express();
 const jsonParser = express.json();
-var pptxFactory = require('./lib/pptxFactory.js');
-var fileVerification = require('./lib/fileVerification.js');
 
 
 app.use(express.static('./public'));
 
+//route to form
 app.get('/', function (request, response) {
     response.sendFile(__dirname + './public/index.html');
 });
 
+//route to download
+app.get('/download', function (request, response) {
+    let filePath = './storage/app.pptx';
+    response.download(filePath);
+});
+
+//request route
 app.post('/request', jsonParser, function (request, response) {
+    let jsonRequest = request.body;
+    let pptxPath = './storage/app.pptx';
+
+    //check for the existence of a request
     if (!request.body) return response.send({
         condition: 'No request'
     });
 
-    let jsonRequest = request.body;
-    let pptxPath = './storage/app.pptx';
-    
+    //delete old file if exist
     fileVerification.ifExistsDelete(pptxPath);
 
     /* DEBUG */
     // console.log('index: ');
     // console.dir(request.body);
 
+    //give request and create presentation
     pptxFactory.get(jsonRequest, response);
 });
 
-app.get('/download', function (request, response) {
-    const filePath = './storage/app.pptx';
-    response.download(filePath);
-});
+
 
 app.listen(3000, function () {
     console.log('Start to 127.0.0.1:3000');
--- public/ajaxPostRequest.js
+++ public/ajaxPostRequest.js
@@ -1,18 +1,30 @@
 $("#submit").click(function (button) {
     button.preventDefault();
 
+    {
+        //clear div containers
         $('#stateContainer').empty();
         $('#downloadPlace').empty();
+        $('#submit').empty();
 
+        //edit loading button
+        $('#submit').append(`
+            <div class="spinner-border" role="status"> 
+                <span class="visually-hidden">Loading...</span>
+            </div>`
+        );
+        $('#submit').addClass('disabled');
+    }
 
+    //init json data object
     let jsonData = {};
 
-    //keys
+    //add keys in json data
     for (let i = 0; i < parseInt($('#countSlide').text()); i++) {
         jsonData[i] = {};
     }
 
-    //attributes
+    //add attributes in json data
     for (let key in jsonData) {
         let headerVar = $(`input[name=headersList${key}]`).val();
         let textVar = $(`textarea[name=textsList${key}]`).val();
@@ -26,25 +38,18 @@ $("#submit").click(function (button) {
     /* DEBUG */
     // console.dir(jsonData);
 
-    let jsonDataToString = JSON.stringify(jsonData);
-
-    {
-        $('#submit').empty();
-        $('#submit').append(`<div class="spinner-border" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>`);
-        $('#submit').addClass('disabled');
-    }
+    //parse json to string
+    const jsonDataToString = JSON.stringify(jsonData);
 
     $.ajax({
         type: "post",
         url: "/request",
         data: jsonDataToString,
         dataType: "json",
-        headers: {
-            "Content-Type": "application/json"
-        }
+        headers: { "Content-Type": "application/json" }
     }).done(function (response) {
+        const serverResponse = response.condition;
+
         $('#submit').empty();
         $('#submit').text('Create');
         $('#submit').removeClass('disabled');
@@ -53,33 +58,44 @@ $("#submit").click(function (button) {
             $('#stateContainer').empty();
         }
 
-        if (response.condition == 'success') {
-            $('#downloadPlace').append(`
-                <div class="d-grid gap-2">
-                <a href="/download" style="background-color: darkorange; border: darkorange;" id="addSlideForm"
-                    class="btn btn-primary mb-3 mt-2" type="button">Download</a>
-                </div>
-            `);
-            $('#stateContainer').append(`
-                <div class="alert bg bg-secondary mb-0" role="alert">
-                    <h2 class="text-white text-center">Success: </h2>
-                    <h3 class="text-white text-center">Click on download button</h3>
-                </div>
-            `);
-        } else if (response.condition == 'No request') {
-            $('#stateContainer').append(`
-            <div class="alert bg bg-danger" role="alert">
-                <h2 class="text-white text-center">Error: </h2>
-                <h3 class="text-white text-center">Error in request</h3>
-            </div>`);
-        } else if (response.condition == 'Error in save pptx') {
-            $('#stateContainer').append(`
-            <div class="alert bg bg-danger" role="alert">
-                <h2 class="text-white text-center">Error: </h2>
-                <h3 class="text-white text-center">Server error</h3>
-            </div>`);
+        //add download button or add Error text
+        switch (serverResponse) {
+            case 'success':
+                $('#downloadPlace').append(`
+                    <div class="d-grid gap-2">
+                    <a href="/download" style="background-color: darkorange; border: darkorange;" id="addSlideForm"
+                        class="btn btn-primary mb-3 mt-2" type="button">Download</a>
+                    </div>
+                `); 
+                break;
+
+            case 'No request':
+                $('#stateContainer').append(`
+                    <div class="alert bg bg-danger" role="alert">
+                        <h2 class="text-white text-center">Error: </h2>
+                        <h3 class="text-white text-center">Error in request</h3>
+                    </div>`
+                );
+                break;
+
+            case 'Error in save pptx':
+                $('#stateContainer').append(`
+                    <div class="alert bg bg-danger" role="alert">
+                        <h2 class="text-white text-center">Error: </h2>
+                        <h3 class="text-white text-center">Server error</h3>
+                    </div>`
+                );
+                break;
+        
+            default:
+                $('#stateContainer').append(`
+                    <div class="alert bg bg-danger" role="alert">
+                        <h2 class="text-white text-center">Error: </h2>
+                        <h3 class="text-white text-center">Server error</h3>
+                    </div>`
+                );
+                break;
         }
 
     });
-
 });
